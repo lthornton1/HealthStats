84a1e77954a46eaaac1f704516795e68e7050da9
lthornton1 <lthornton1@mail.csuchico.edu>
2018-03-08 14:48:16 -0800

    healthstats with extra credit

--- main.cpp
+++ main.cpp
@@ -6,9 +6,43 @@ using namespace std;
 
 int main()
 {
-    vector <int> bpm;
-    vector <int> steps;
-    vector <int> calories;
+  vector <int> bpm;
+  vector <int> steps;
+  vector <int> calories;
+/*
+  bpm.resize(10);
+  bpm.push_back(90);
+  bpm.push_back(100);    
+  int length = bpm.size();
+*/
+int rate;
+int highest;
 
-    return 0;
+ // cout << "bpm is currently size, " << length <<endl;
+//  cout << "The last item in bpm is ..." << bpm[length-1] << endl;
+  cout << "The items in bpm are ..." << endl;
+  for (int i=0; i<bpm.size(); i++){
+    cout << bpm[i] << endl;
+  }
+
+  do{
+    cout << "please" << endl;
+    cin >>rate;
+    bpm.push_back(rate);
+  }while(rate != -1);
+
+
+for (int i=1; i<bpm.size(); i++){
+  if ( bpm[i-1] < bpm[i]){
+    highest = bpm[i];
+  }
+}
+
+cout << highest<<endl;
+
+
+
+
+
+  return 0;
 }
